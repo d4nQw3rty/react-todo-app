dfcce5059a0ccb8f9c97eb6d48af91e909adf04a
d4nQw3rty <danlinmoy@gmail.com>
2022-10-30 12:33:22 -0300

    Delete items from the todos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,8 +6,11 @@ function TodoItem(props) {
       <input
         type="checkbox"
         checked={props.todo.completed}
-        onChange={() => props.handleChangeProps(props.todo.id)}             
+        onChange={() => props.handleChangeProps(props.todo.id)}                   
       />
+      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
+  Delete
+</button>
       {props.todo.title}
     </li>
   );
