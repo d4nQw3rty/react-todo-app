de104a60f9e37ad4c975eac005dcbc76b07c5227
d4nQw3rty <danlinmoy@gmail.com>
2022-10-31 14:21:14 -0300

    Persist the todos data to local storage

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -8,13 +8,22 @@ class TodoContainer extends React.Component {
   state = {
     todos: [],
   };
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
-      .then(response => response.json())
-      .then(data => this.setState({ todos: data }));
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
   }
-
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  }
+  
   handleChange = (id) => {
     this.setState((prevState) => ({
       todos: prevState.todos.map((todo) => {
