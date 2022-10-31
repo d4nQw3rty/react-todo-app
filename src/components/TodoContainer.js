7d0e1220ab58b3585589be94d5679c8c59de8c6f
d4nQw3rty <danlinmoy@gmail.com>
2022-10-31 11:13:02 -0300

    Styling React App with CSS Modules
    -Add Styles when any of the todos items is completed.
    -Use Destructuring

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Header from './Header';
 import TodosList from './TodosList';
-import InputTodo from "./InputTodo";
-import { v4 as uuidv4 } from "uuid";
+import InputTodo from './InputTodo';
 
 class TodoContainer extends React.Component {
   state = {
@@ -44,27 +44,29 @@ class TodoContainer extends React.Component {
     });
   };
 
-  addTodoItem = title => {
+  addTodoItem = (title) => {
     const newTodo = {
       id: uuidv4(),
-      title: title,
-      completed: false
+      title,
+      completed: false,
     };
     this.setState({
-      todos: [...this.state.todos, newTodo]
+      todos: [...this.state.todos, newTodo],
     });
   };
 
   render() {
     return (
-      <div>
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-        />
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 // component file
 import TodoContainer from './components/TodoContainer';
 
+// stylesheet
+import './App.css';
+
 ReactDOM.render(
-//  <React.StrictMode>
+  // <React.StrictMode>
   <TodoContainer />,
   // </React.StrictMode>,
   document.getElementById('root'),
