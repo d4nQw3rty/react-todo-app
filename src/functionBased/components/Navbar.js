075d06ac75ceb828af8ea7b20fd488512819ede7
d4nQw3rty <danlinmoy@gmail.com>
2022-11-01 16:17:07 -0300

    change button state open and close

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   const links = [
     {
       id: 1,
@@ -14,8 +15,13 @@ const Navbar = () => {
       text: 'About',
     },
   ];
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
   return (
     <nav className="navBar">
+      <button type="button" onClick={handleToggle}>{navbarOpen ? 'Close' : 'Open'}</button>
       <ul>
         {links.map((link) => (
           <li key={link.id}>
